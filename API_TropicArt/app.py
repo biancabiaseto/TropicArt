35af789b1a1120d4c824fe648b869129448259e9
Bianca Biaseto <bianca.biaseto@aluno.ifsp.edu.br>
2025-06-20 16:39:51 -0300

    primeiro commit

---
+++ API_TropicArt/app.py
@@ -0,0 +1,2 @@
+from flask import Flask
+from routes.art_routes import 
---
+++ API_TropicArt/models/art.py
@@ -0,0 +1,9 @@
+class Artista:
+    def __init__(self, nome, userName, senha, area, email, telefone):
+        self.nome = nome
+        self.userName = userName
+        self.senha = senha
+        self.area = area
+        self.email = email
+        self.telefone = telefone
+        
